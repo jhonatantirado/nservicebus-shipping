a857761df1444b539d460258c9a6ab3fb3ccf9bc
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-10-29 23:14:09 -0500

    SendGrid

---
+++ Shipping.EndPoint/SendGridEmail.cs
@@ -0,0 +1,32 @@
+﻿using NServiceBus.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Shipping.EndPoint
+{
+    class SendGridEmail
+    {
+        static ILog log = LogManager.GetLogger<SendGridEmail>();
+
+        public static void Submit(string sender, string receiver,string subject)
+        {
+            Execute(sender, receiver, subject).Wait();
+        }
+
+        static async Task Execute(string sender, string receiver, string mailSubject)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("Morita-SendGridAPIKey");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(sender, sender);
+            var subject = mailSubject;
+            var to = new EmailAddress(receiver, receiver);
+            var plainTextContent = "SendGrid is awesome";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            log.Info("Email submitted");
+        }
+    }
+}
--- Shipping.EndPoint/ShipOrderHandler.cs
+++ Shipping.EndPoint/ShipOrderHandler.cs
@@ -8,10 +8,13 @@ namespace Shipping.EndPoint
     public class ShipOrderHandler : IHandleMessages<ShipOrderCommand>
     {
         static ILog log = LogManager.GetLogger<ShipOrderHandler>();
+        static readonly string sender = "jhonatantiradotiradodeep@gmail.com";
+        static readonly string receiver = "jhonatan.tirado@unmsm.edu.pe";
 
         public Task Handle(ShipOrderCommand message, IMessageHandlerContext context)
         {
             log.Info($"Order [{message.OrderId}] - Succesfully shipped.");
+            SendGridEmail.Submit(sender, receiver, $"Order [{message.OrderId}] - Succesfully shipped.");
             return Task.CompletedTask;
         }
     }
